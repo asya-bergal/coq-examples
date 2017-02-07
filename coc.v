9d5875cf0916263dc01e038561ad128ef9df0ddf
Asya Bergal <asyabergal@gmail.com>
2017-02-07 16:28:14 -0500

    Last commit before blowing away dependent types

--- coc.v
+++ coc.v
@@ -5,17 +5,22 @@ Require Import Arith.
 
 Infix "==n" := eq_nat_dec (no associativity, at level 50).
 
+(* False is forall T, T. (produces thing of type T for any T), exfalso implemented this way *)
+(* coq examples: dependent types lists vs list parametrized by length, writing append function, head vs head with proof *)
+
+(* Definition head_with_proof T (l : list T) (H : l <> []) : T. *)
+(*   destruct l as [_ | hd tl]. *)
+(*   destruct (H eq_refl). *)
+(*   exact t. *)
+(* Defined. *)
+
 Inductive var : nat -> Set :=
 | Var : forall n, var n.
-
-(* Context (termType: Set). *)
-(* Context {ctype cprop capp cfun cforall : termType}. *)
-(* Context {cvar: nat -> termType}. *)
 
 Inductive termType : Type :=
 | ctype
 | cprop
-| cvar (n : nat)
+| cvar (_ : nat)
 | capp
 | cfun
 | cforall.
@@ -25,7 +30,7 @@ Inductive term : termType -> Type :=
 | cType : term ctype
 | cProp : term cprop
 | cVar {n} (_ :var n) : term (cvar n)
-| cApp {c1 c2} (_:term c1) (_:term c2) : term capp
+| cApp {c} (_:term cfun) (_:term c) : term capp
 | cFun {c1 c2 n} (_:term (cvar n)) (_:term c1) (_:term c2) : term cfun
 | cForall {c1 c2 n} (_:term (cvar n)) (_:term c1) (_:term c2) : term cforall.
 
@@ -33,29 +38,43 @@ Context (hasType : forall {t T : termType}, term t -> term T -> Prop).
 
 Delimit Scope coc_scope with coc.
 
-Print Grammar constr.
 Notation "A : B" := (hasType _ _ A B) (at level 50) : coc_scope.
 
 Context (judgement : list Prop -> list Prop -> Prop).
 Notation "A |- B" := (judgement A B) (at level 95) : coc_scope.
 
-(* TODO: Ask Peter about cvar assumption *)
+(* B(x := N) *)
 Fixpoint substitute {tB tN} (B : term tB) {n} (x : term (cvar n)) (N : term tN):
   term match B with
-       | cType => tB
-       | cProp => tB
        | @cVar n' _ => if n' ==n n then tN else tB
-       | cApp _ _ => tB
-       | cFun _ _ _ => tB
-       | cForall _ _ _ => tB
-       end :=
+       | _ => tB
+       end.
+  destruct B; try constructor.
+  destruct (n0 ==n n).
+  exact N.
+  exact (cVar v).
+  refine (cApp _ _).
+  assert (
+      match B1 with
+      | cType => cfun
+      | cProp => cfun
+      | @cVar n' _ => if n' ==n n then tN else cfun
+      | cApp _ _ => cfun
+      | cFun _ _ _ => cfun
+      | cForall _ _ _ => cfun
+      end = cfun).
+  destruct B1.
+
+  refine (substitute cfun tN B1 n x N).
+
+  exact (cApp (substitute c1 tN B1 n x N) (substitute c2 tN B2 n x N)).
+  (* TODO: ask peter about this *)
+  exact (cFun B1 (substitute c1 tN B2 n x N) (substitute c2 tN B3 n x N)).
+  exact (cForall B1 (substitute c1 tN B2 n x N) (substitute c2 tN B3 n x N)).
+
   match B as t' in (term t) return (term match t' with
-                                         | cType => t
-                                         | cProp => t
                                          | @cVar n' _ => if n' ==n n then tN else t
-                                         | cApp _ _ => t
-                                         | cFun _ _ _ => t
-                                         | cForall _ _ _ => t
+                                         | _ => t
                                          end) with
   | cType => cType
   | cProp => cProp
@@ -65,22 +84,21 @@ Fixpoint substitute {tB tN} (B : term tB) {n} (x : term (cvar n)) (N : term tN):
   | cForall B1 B2 B3 => cForall B1 (substitute B2 x N) (substitute B3 x N)
   end.
 
-(* the fact that I have to do this in proof mode probably means I have too many dependent types *)
-(* Fixpoint substitute {tB tN} (B : term tB) {n} (x : term (cvar n)) (N : term tN) : *)
-(*   term (match B with *)
-(*         | @cVar n' _ => if n' ==n n then tN else tB *)
-(*         | _ => tB *)
-(*         end). *)
-(*   destruct B; try constructor. *)
-(*   destruct (n0 ==n n). *)
-(*   exact N. *)
-(*   exact (cVar v). *)
-(*   exact (cApp (substitute c1 tN B1 n x N) (substitute c2 tN B2 n x N)). *)
-(*   (* TODO: ask peter about this *) *)
-(*   exact (cFun B1 (substitute c1 tN B2 n x N) (substitute c2 tN B3 n x N)). *)
-(*   exact (cForall B1 (substitute c1 tN B2 n x N) (substitute c2 tN B3 n x N)). *)
-(* Defined. *)
+(* Fixpoint beta_reduce {T} (t: term T) : *)
+(*   term match t with *)
+(*        | cApp func args =>  *)
+(*          match func with *)
+(*          |  *)
+(*          | _ => T *)
 
+(*        | _ => T *)
+(*        end. *)
+  
+
+
+  (* application lambda thing of type A *)
+
+  (* TODO: Guarantee that x is fresh in A in 2, in 4 variables of N should be disjoint with variables of B, freshness constraints all over the damn place*)
 Axiom (introduction : forall (gamma : list Prop) (P : term cprop) (T : term ctype), 
           (gamma |- [P : T])%coc).
 
@@ -90,6 +108,7 @@ Axiom (weakening_type : forall (gamma : list Prop) {tA} (A : term tA) (K : term 
 Axiom (weakening_prop : forall (gamma : list Prop) {t} (A : term t) (K : term cprop) {n} (x : term (cvar n)),
           (x : A :: gamma |- [x : A])%coc).
 
+(* TODO : t is an arbitrary term, loloops *)
 Axiom (lambda_type : forall (gamma : list Prop) {tA tB} (A : term tA) (B : term tB) {n1 n2} (x : term (cvar n1)) (t : term (cvar n2)) (K : term ctype),
           n1 <> n2 ->
           (x : A :: gamma |- [t : B ; B : K])%coc ->
@@ -111,36 +130,3 @@ Axiom (eval : forall (gamma : list Prop) {tM tN tA tB} (M : term tM) (N : term t
           (gamma |- [M : cForall x A B])%coc ->
           (gamma |- [N : A])%coc ->
           (gamma |- [MN : substitute B x N])%coc).
-
-
-
-
-(* Definition hasType (term t)  *)
-
-
-
-(* Inductive assig : Set := *)
-(*   | Assig : forall (t T : term), hasType t T -> assig. *)
-
-
-
-(* Definition isProp (t : term) : Prop := *)
-(*   match t with *)
-(*   | cProp => True *)
-(*   | _ => False *)
-(*   end. *)
-
-(* Definition isType (t : term) : Prop := *)
-(*   match t with *)
-(*   | cType => True *)
-(*   | _ => False *)
-(*   end. *)
-
-(* Definition isVar (t : term) : Prop := *)
-(*   match t with *)
-(*   | cVar _ _ => True *)
-(*   | _ => False *)
-(*   end. *)
-
-(* Definition difVars (x y : term) (H1 : isVar x) (H2: isVar y) : Prop. *)
-(*   unfold isVar in H1, H2. *)
